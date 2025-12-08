82a32dad58e5fc7793949514c33ddc47f2718eaf
Sunwoo Park <lunatic230139@gmail.com>
2025-12-08 10:53:52 +0900

    UI수정

--- GameEngine_Final_Project/Assets/Final_project/Scripts/UI_Scripts/MainMenu.cs
+++ GameEngine_Final_Project/Assets/Final_project/Scripts/UI_Scripts/MainMenu.cs
@@ -1,36 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UI; 
 
 public class MainMenu : MonoBehaviour
 {
-    public Button continueButton; // 인스펙터에서 이어하기 버튼 연결
+    public Button continueButton; 
 
     void Start()
     {
-        // 저장된 데이터가 없으면 이어하기 버튼을 꺼버림 (비활성화)
+        // 저장된 데이터가 없으면 이어하기 버튼 끄기
         if (!PlayerPrefs.HasKey("HasSaveData"))
         {
             continueButton.interactable = false; 
-            // 또는 continueButton.gameObject.SetActive(false); 로 아예 숨김
         }
     }
 
     public void OnContinueGame()
     {
-        // 매니저에게 "저장된 거 불러와!" 명령
         if (GameManager.instance != null)
         {
             GameManager.instance.LoadGameData();
         }
     }
 
-    public void OnNewGame() // 기존 시작 버튼
+    public void OnNewGame()
     {
-        // 새 게임이니까 기존 저장 데이터 삭제 (선택사항)
         PlayerPrefs.DeleteAll(); 
-        
-        // 기본 씬 로드
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Tutorial"); // 첫 맵 이름
+        SceneManager.LoadScene("Tutorial"); 
+    }
+
+    // ★ [추가됨] 게임 종료 함수
+    public void OnQuitGame()
+    {
+        Debug.Log("게임 종료!"); // 에디터에서 눌렀는지 확인용 로그
+
+        // 유니티 에디터에서 플레이 중일 때는 플레이 모드를 멈춤
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // 실제 빌드된 게임(exe 파일 등)에서는 창을 닫음
+            Application.Quit();
+        #endif
     }
 }

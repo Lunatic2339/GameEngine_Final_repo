35caa723396274a0e9d31d1fa7be155e74014b4b
Sunwoo Park <lunatic230139@gmail.com>
2025-12-19 05:21:12 +0900

    중간

--- GameEngine_Final_Project/Assets/Final_project/Scripts/PlatformScripts/Trap.cs
+++ GameEngine_Final_Project/Assets/Final_project/Scripts/PlatformScripts/Trap.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("트랩 모드 설정")]
+    [Tooltip("체크하면 닿자마자 즉사합니다 (Die 호출)")]
+    public bool isInstantKill = false;
 
-    // Update is called once per frame
-    void Update()
+    [Tooltip("즉사가 아닐 때 입힐 데미지 양")]
+    public int damage = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            PlayerHealth player = collision.GetComponent<PlayerHealth>();
+
+            if (player != null)
+            {
+                if (isInstantKill)
+                {
+                    // 모드 1: 즉사 (Die 함수 강제 호출)
+                    Debug.Log("즉사 트랩 발동!");
+                    player.Die(); 
+                }
+                else
+                {
+                    // 모드 2: 데미지 (설정된 값만큼만 깎임)
+                    // (밀쳐내는 효과를 위해 transform도 같이 전달)
+                    player.TakeDamage(damage, transform); 
+                }
+            }
+        }
     }
 }
